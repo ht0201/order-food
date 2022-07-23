304d32813642a06edb7c0cbb1ca754c19ac66e82
thai.vo <43738511+ht0201@users.noreply.github.com>
2022-07-23 19:26:10 +0700

    one part cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Cart from './conponents/Cart/Cart';
 import Header from './conponents/Layout/Header';
 import Meals from './conponents/Meals/Meals';
 
 function App() {
+  const [isShownCart, setisShownCart] = useState(false);
+
+  const showCartHandler = () => {
+    setisShownCart(true);
+  };
+
+  const hideCartHandler = () => {
+    setisShownCart(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
+      {isShownCart && <Cart onShowCart={showCartHandler} />}
+
       <Header />
       <main>
         <Meals />
